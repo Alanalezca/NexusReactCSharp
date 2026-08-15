77d84fd71aff64e561243bbad2af55e9efe68f9b
Alanalezca <alana_forever@hotmail.com>
2026-08-15 17:12:19 +0200

    Mise à jour client.ts

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -1,6 +1,6 @@
 // Entrée frontend des appels API
 
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = import.meta.env.VITE_API_URL ?? "";
 // const API_URL = "";   Si chez CB
 
 function getDefaultErrorMessage(status: number) {
